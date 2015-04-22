492643b0c35cdb9e2ce0665f47b3e72787b42806
Shizhe Chen <schen381@users.noreply.github.com>
2015-04-22 16:13:28 -0400

    Update staffHomePage.php

--- staffHomePage.php
+++ staffHomePage.php
@@ -8,7 +8,7 @@
 <h1>Staff Home Page</h1>
 
 <a href="checkOutBook.php">Check Out Book</a>
-<a href="PopularSubjectReport.php">Return Book</a>
+<a href="returnBook.php">Return Book</a>
 <a href="penaltyCharge.php">Penalty Charge</a>
 <a href="DamagedBookReport.php">Damaged Book Report</a>
 <a href="FrequentUserReport.php">Frequent User Report</a>
