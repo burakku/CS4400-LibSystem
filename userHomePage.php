55e1d9581a0e31f9649ca64357d737d018650c78
Shizhe Chen <shizhe95@gmail.com>
2015-04-22 16:58:25 -0400

    user home page

--- userHomePage.php
+++ userHomePage.php
@@ -1,17 +1,49 @@
-
+<!DOCTYPE HTML>
 
 <html>
-  <head>
-  </head>
-<body>
+    <head>
+        <title>Library Manage System</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery.scrollex.min.js"></script>
+        <script src="js/jquery.scrolly.min.js"></script>
+        <script src="js/skel.min.js"></script>
+        <script src="js/init.js"></script>
+        <noscript>
+            <link rel="stylesheet" href="css/skel.css" />
+            <link rel="stylesheet" href="css/style.css" />
+            <link rel="stylesheet" href="css/style-xlarge.css" />
+        </noscript>
+    </head>
+    <body>
 
-<h1>User Home Page</h1>
-
-<a href="SearchBook.php">Search Books</a>
-<a href="RequestExtension.php">Request Extension on a Book</a>
-<a href="FutureHoldRequest.php">Future Hold Request on a Book </a>
-<a href="TrackBook.php">Track Book</a>
-<a href="index.php">Back</a>
-
-</body>
+        <!-- Header -->
+            <section id="header">
+                <header class="major">
+                    <h1>User Home Page</h1>
+                    <p>Let's make reading more fun! <3</p>
+                </header>
+                <div class="container">
+                        <div class="row uniform">
+                            <div class="6u 12u$(xsmall)"><a href="SearchBook.php">Search Books</a></div>
+                            <div class="6u 12u$(xsmall)"><a href="RequestExtension.php">Request Extension on a Book</a></div>
+                            <div class="6u 12u$(xsmall)"><a href="FutureHoldRequest.php">Future Hold Request on a Book </a></div>
+                            <div class="6u 12u$(xsmall)"><a href="TrackBook.php">Track Book</a></div>
+                            <div class="12u$">
+                                <a href="login.php">Back</a>
+                            </div>
+                        </div>
+                </div>
+            </section>
+        
+        <!-- Footer -->
+            <section id="footer" class="main special">
+            <span class="image fit primary"><img src="images/pic01.jpg" alt="" /></span>
+                <footer>
+                </footer>
+            </section>
+    </body>
 </html>
